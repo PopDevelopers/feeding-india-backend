75ee88344af806d4b43dbc0700a2cfc1ce1d9509
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-09-02 22:07:22 +0530

    Super minor: Now also returning donor_type on successful donor login

--- getauth/register_login.php
+++ getauth/register_login.php
@@ -23,7 +23,7 @@ if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['user_typ
 
             if($action == 'login')
             {
-                $query = "SELECT `donor_id`,`password`,`name`,`phone_no_1` FROM `donors` WHERE `email` = '$email';";
+                $query = "SELECT `donor_id`,`password`,`name`,`phone_no_1`,`donor_type` FROM `donors` WHERE `email` = '$email';";
                 $result = mysqli_query($conn, $query);
 
                 if($result == false)
@@ -46,7 +46,7 @@ if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['user_typ
 
                         if($password_check_result == true)
                         {
-                            $response = array('response_code'=>1,'message'=>'Welcome back','id'=>$row['donor_id'],'name'=>$row['name'],'password_hash'=>$row['password'],'phone_no_1'=>$row['phone_no_1']);
+                            $response = array('response_code'=>1,'message'=>'Welcome back','id'=>$row['donor_id'],'name'=>$row['name'],'password_hash'=>$row['password'],'phone_no_1'=>$row['phone_no_1'],'donor_type'=>$row['donor_type']);
                         }
                         else
                         {
