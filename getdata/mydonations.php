214a24a6fafec2d8aa18c66b8df09d184ab69269
Abhishek Popli <abhishekpopli80@gmail.com>
2017-09-12 22:16:01 +0530

    mydonations.php added

--- getdata/mydonations.php
+++ getdata/mydonations.php
@@ -2,29 +2,29 @@
     require '../include/connect.php';
 
     // Variables
-    $limit = 10;
+    
     $donor_id = $_POST['donor_id'];
 	$query = "SELECT `donation_id`,`name`,`photo_url`,TIME_FORMAT(TIME(request_datetime), '%h:%i %p')
 		AS `request_datetime`,TIME_FORMAT(TIME(pickup_datetime), '%h:%i %p') AS `pickup_datetime`,
 		`pickup_area`,`is_perishable`,`is_veg`,`is_picked`,`is_accepted`,`other_details` FROM `donations`, 
-		`donors` WHERE ((donations.`donor_id` = donors.`donor_id`) AND (donors.`donor_id` = '$donor_id')) ORDER BY `donation_id` DESC LIMIT $limit;";
+		`donors` WHERE ((donations.`donor_id` = donors.`donor_id`) AND (donors.donor_id = '$donor_id')) ORDER BY `donation_id` ;";
     
     $result = mysqli_query($conn, $query);
     
-    if($result == false)
+    if(!$result)
     {
         //die("Query cannot be run");
+		echo("it looks as if there is a problem");
         die(mysqli_error($conn));
     }
     else
     {
-        $array = array();
+        $response = array();
         while ($row = mysqli_fetch_assoc($result))
         {
-            array_push($array,$row);
+            array_push($response,$row);
         }
-        header('Content-Type:Application/json');
-        echo json_encode($array);
+        echo json_encode($response);
     }
     mysqli_close($conn);   
 ?>
