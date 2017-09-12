7cb1c4e6161bfb14a22aaebd88e9634c1bbe08ca
Abhishek Popli <abhishekpopli80@gmail.com>
2017-09-12 22:21:59 +0530

    mydonations with scrollview limit

--- getdata/mydonations.php
+++ getdata/mydonations.php
@@ -2,12 +2,27 @@
     require '../include/connect.php';
 
     // Variables
+        $limit = 10;
+    if(isset($_POST['donation_id']))
+    {
+        $donation_id = $_POST['donation_id'];
+        
+        if($donation_id < 1)
+        {
+            $donation_id = PHP_INT_MAX;
+        }
+    }
+    else
+    {
+        $donation_id = PHP_INT_MAX;
+    }
+    
     
     $donor_id = $_POST['donor_id'];
 	$query = "SELECT `donation_id`,`name`,`photo_url`,TIME_FORMAT(TIME(request_datetime), '%h:%i %p')
 		AS `request_datetime`,TIME_FORMAT(TIME(pickup_datetime), '%h:%i %p') AS `pickup_datetime`,
 		`pickup_area`,`is_perishable`,`is_veg`,`is_picked`,`is_accepted`,`other_details` FROM `donations`, 
-		`donors` WHERE ((donations.`donor_id` = donors.`donor_id`) AND (donors.donor_id = '$donor_id')) ORDER BY `donation_id` ;";
+		`donors` WHERE ((donations.`donor_id` = donors.`donor_id`) AND(`donation_id` < $donation_id) AND  (donors.donor_id = '$donor_id')) ORDER BY `donation_id` DESC LIMIT $limit ;";
     
     $result = mysqli_query($conn, $query);
     
